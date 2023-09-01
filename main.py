84ac6fdcfcc04f47cf682382c3a95c2618291e44
Taro Kimura <tarokimuraorg@outlook.com>
2023-09-01 11:39:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from JPPhoneNumberBuilder import JPPhoneNumberBuilder
 if __name__ == "__main__":
 
     try:
-        pnbuilder = JPPhoneNumberBuilder('0139456789')        
+        pnbuilder = JPPhoneNumberBuilder('0150756789')
+        print(pnbuilder.digits_only())
+        
         phone_number = pnbuilder.hyphenated()
         print(phone_number)
         
